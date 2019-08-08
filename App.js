dfa8cb335d023b7d111c0238849f9e52972a379e
Goodup302 <j.f0471430704@gmail.com>
2019-08-08 17:09:48 +0200

    Add Infinite Scroll and update style

--- App.js
+++ App.js
@@ -1,44 +1,58 @@
 import React from 'react';
 import {AppRegistry, StyleSheet, Text, View, Platform, Button, ImageBackground, FlatList} from 'react-native';
 import Image from './assets/font-home.jpg';
-import {API, API_TOKEN, getToken} from "./src/Api";
-
-const request = async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos/');
-    const json = await response.json();
-    console.log(json);
-};
+import Api from "./src/Api";
+import ProductListItem from "./src/components/ProductListItem";
 
 export default class App extends React.Component{
+    state = {
+        items: null,
+        loading: false,
+    };
+
     constructor(props) {
         super(props);
-        this.state = {
-            items: API.getProductList()
-        };
-        API.getProductList().then((res) =>
-            this.state = {
-                items: res.data.attributes
-            }
-        );
-        console.log(this.state.items)
-
-        // console.log(API.getToken().then(data => console.log(data)))
-        // console.log(API_TOKEN)
-        //request();
+        Api.tokenIsfind().then(() => {
+            Api.getProductList().then((result) => {
+                this.setState({
+                    items: result.data,
+                    links: result.links
+                })
+            });
+        });
     }
 
-    render () {
+    loadMore = () => {
+        if (this.state.links['next'] && !this.state.loading) {
+            this.setState({loading: true});
+            Api.tokenIsfind().then(() => {
+                Api.getProductList(this.state.links['next']).then((res) => {
+                    this.setState({
+                        items: [ ...this.state.items, ...res.data],
+                        links: res.links,
+                        loading: false
+                    });
+                });
+            });
+        }
+    };
+
+    render() {
         return (
             <ImageBackground source={Image} style={styles.backgroundImage}>
                 <View style={styles.titleContainer}>
-                    <Text style={styles.text}>BileMo</Text>
-                    <Button title="Rechercher" onPress={() => {}}/>
+                    <Text style={styles.title}>BileMo</Text>
                 </View>
-
-                <FlatList
-                    data={this.state.items}
-                    renderItem={({item}) => <Text>{item.id}</Text>}
-                />
+                <View style={styles.listContainer}>
+                    <FlatList
+                        style={styles.list}
+                        keyExtractor={(item) => item.id.toString()}
+                        data={this.state.items}
+                        renderItem={({item}) => <ProductListItem data={item}/>}
+                        onEndReachedThreshold={0.1}
+                        onEndReached={() => this.loadMore()}
+                    />
+                </View>
             </ImageBackground>
         );
     }
@@ -52,11 +66,28 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     titleContainer: {
-        flex: 0.5,
+        flex: 0.2,
+        width: '100%',
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
         //backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    },
+    listContainer: {
+        flex: 0.8,
+        width: '100%',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        //backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    },
+    list: {
+        width: '100%',
+    },
+    title: {
+        fontSize: 60,
+        color: 'white',
+        fontWeight: 'bold',
     },
     text: {
         color: 'white',
@@ -64,9 +95,8 @@ const styles = StyleSheet.create({
     }
 });
 
-
 if (Platform.OS === 'web') {
-    AppRegistry.registerComponent('App', () => App);
+    //AppRegistry.registerComponent('App', () => App);
     AppRegistry.runApplication('App', {
         rootTag: document.getElementById('root')
     });
--- src/Api.js
+++ src/Api.js
@@ -1,36 +1,37 @@
-export let API_TOKEN = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE1NjUwMTc3MDEsImV4cCI6MTU2NTEwNDEwMSwicm9sZXMiOlsiUk9MRV9VU0VSIl0sInVzZXJuYW1lIjoiU0ZSLTVkNDgzZGNiMGFkNjUifQ.CwNr1NhFjTOI88EOScneUk2UtOSFIRjIi7PUWO7pQCbkVReMIzcy1iZLHq2PjuSlsWOgbZD-oY8J3dey4DxFgUPHZdKqJzDJ2ZnUGxLLX96FKjZK8nrihD645IJwfJBGJRthx5_dOYdzOiETNquq1ORE8TjDAeMMO-mhY86xIzUVOY6dWZFwfuQ4JMG2NFESHNgjVXVtIGpf7qNNZIEai0btXwdDugjqXLhyalEO1dnJiQ8mwvC0Cj-NlObKaHcG7gHVEa22dxIdr8F-tfn-jORUJ6shyer8ra5cVtqmsq_HgEsGa-nTTzuOnRcs_c3MprY95JxsLhBeWwMLXSUf3VTb0bHJ71XmwudPuM0lkhAjsFr6plkwrvAMf2AtuuNnhEyaMiZCY4FPCqV61kne1deNbK7KHRxfI9kYtiGtvkRgyrp0UW_jBtsYQhuMvqbGPpxis1VfKjMeQk-Pvt-SnHKFIaZ1SGHUaS5qZP6L1bl_hlnxyb2yKBedDtt7Ez105evEg_9uGO_-Gb9lKSBbb6K1XNawp-w8s_YfmvcrXwYtreGokTU5XRaxPXc3OFDFVa5Eri4N9WvgRbB4J0YED4atEXGAdUlgWurkfqY9Mkzjkq_HNZFubstNzCGntmvQJdOA317K4AV_K5VDkBnALrQ0Te4uSNiKiYPwIfHiR5k';
-export const API_URL = 'http://bilemo.goodup302.fr';
 export const API_LOGIN = {
-    username: "SFR-5d483dcb0ad65",
+    username: "Bouygues Telecom-5d4c287914a99",
     password: "admin"
 };
 
+export default class Api {
+    static token;
+    static BASE_URL = 'http://bilemo.goodup302.fr';
 
-class Api {
-    constructor() {
-        this.getToken()
+    static tokenIsfind() {
+        if (!Api.token) {
+            return fetch(Api.BASE_URL+'/token', {
+                headers: {'Content-Type': 'application/json'},
+                method: "POST",
+                body: JSON.stringify(API_LOGIN)
+            })
+                .then((res) => res.json())
+                .then((res) =>
+                    Api.token = 'Bearer '+res.token
+                ).catch((error) => console.error(error))
+        } else {
+            return new Promise((resolve) => {
+                resolve(Api.token)
+            });
+        }
     }
 
-    getToken() {
-        return fetch(API_URL+'/token', {
-            headers: {'Content-Type': 'application/json'},
-            method: "POST",
-            body: JSON.stringify(API_LOGIN)
-        })
-            .then((res) => res.json())
-            .then((res) => {
-                API_TOKEN = 'Bearer '+res.token;
-                //console.log(API_TOKEN);
-            }).catch((error) => console.error(error))
-    }
-
-    getProductList(link = '/api/products?page=1') {
-        //console.log('token: '+API_TOKEN);
-        return fetch(API_URL+link, {
+    static getProductList(link = '/api/products?page=1') {
+        //console.log(link);
+        return fetch(Api.BASE_URL+link, {
             headers: {
                 'Accept': 'application/vnd.api+json',
                 'Content-Type': 'application/json',
-                'Authorization': API_TOKEN,
+                'Authorization': Api.token,
             },
             method: "GET",
         })
@@ -38,12 +39,12 @@ class Api {
             .catch((error) => console.error(error))
     }
 
-    getSingleProduct(link) {
-        return fetch(API_URL+link, {
+    static getSingleProduct(link) {
+        return fetch(Api.BASE_URL+link, {
             headers: {
                 'Accept': 'application/vnd.api+json',
                 'Content-Type': 'application/json',
-                'Authorization': API_TOKEN,
+                'Authorization': Api.token,
             },
             method: "GET",
         })
@@ -51,5 +52,3 @@ class Api {
             .catch((error) => console.error(error))
     }
 }
-
-export let API = new Api();
